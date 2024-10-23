5a8d70bbbda23355da3788840aaaeee7de7460bf
İbrahim Yıldız <93480258+ibrahimyz@users.noreply.github.com>
2024-10-23 12:22:00 +0300

    Add files via upload

---
+++ CityExplorer/src/CityExplorer.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+#include "../include/headers/Vector.h"
+#include "../include/algorithm/CityExplorer.h"
+
+// Create an instance of the Graph class
+algorithm::Graph G1;
+
+int main() {
+
+	// Perform Depth-First Search (DFS) from each city
+	for (size_t src = 0; src < 81; ++src) {
+		G1.DFSFromSource(src); // Explore all paths starting from the current city
+	}
+	// Find and print the maximum path length among all cities
+	G1.findMaxLength();
+
+	return 0;
+}
+
