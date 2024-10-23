169d445308998130b3f04b31e539947f1f6b26a8
İbrahim Yıldız <93480258+ibrahimyz@users.noreply.github.com>
2024-10-23 12:19:59 +0300

    Add files via upload

---
+++ BusScheduling/include/util/ErrorCode.h
@@ -0,0 +1,16 @@
+#ifndef INCLUDE_UTIL_ERRORCODE_H_
+#define INCLUDE_UTIL_ERRORCODE_H_
+
+namespace util {
+
+	// Enum class for error codes used in the application
+
+	enum ErrorCode {
+		NO_ERROR = 0,
+		NO_AVAILABLE_OBJECT = -1,
+		OBJECT_NOT_ALLOCATED = -2,
+		OBJECT_NOT_FOUND = -3
+	};
+}
+
+#endif // !INCLUDE_UTIL_ERRORCODE_H_
